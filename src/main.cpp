44614d03c70ea25b1f8a345a8f0ffb2ada17f4ad
Ivan Rusev <ivanrusev03@schoolmath.eu>
2025-04-26 21:51:02 +0300

    feat: add basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-#include "MiniBrowser.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MiniBrowser w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
